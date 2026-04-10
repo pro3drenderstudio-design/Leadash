f1ecf35b3b910c5a8e1453a729d30d727f0a0dbd
Malik Adelaja <pro3drenderstudio@gmail.com>
2026-04-10 21:27:38 +0200

    auto-deploy

--- apps/web/src/components/Sidebar.tsx
+++ apps/web/src/components/Sidebar.tsx
@@ -1,5 +1,6 @@
 "use client";
 import Link from "next/link";
+import Image from "next/image";
 import { usePathname } from "next/navigation";
 import { createClient } from "@/lib/supabase/client";
 
