771af20298c3ffffba7f92b396e7e32b3ea8c3f8
Malik Adelaja <pro3drenderstudio@gmail.com>
2026-04-16 06:43:34 +0200

    auto-deploy

--- apps/web/src/app/(marketing)/layout.tsx
+++ apps/web/src/app/(marketing)/layout.tsx
@@ -1,3 +1,14 @@
+import SiteNav from "@/components/SiteNav";
+import SiteFooter from "@/components/SiteFooter";
+
 export default function MarketingLayout({ children }: { children: React.ReactNode }) {
-  return <>{children}</>;
+  return (
+    <div className="min-h-screen flex flex-col" style={{ background: "#020617" }}>
+      <SiteNav />
+      <div className="pt-16 flex-1">
+        {children}
+      </div>
+      <SiteFooter />
+    </div>
+  );
 }
