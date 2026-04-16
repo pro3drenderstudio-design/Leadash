68502a5d167f33c99b64ba148750379364e09b3e
Malik Adelaja <pro3drenderstudio@gmail.com>
2026-04-16 23:44:22 +0200

    auto-deploy

--- apps/worker/src/workers/reply-worker.ts
+++ apps/worker/src/workers/reply-worker.ts
@@ -7,7 +7,7 @@ export interface ReplyJobData {
 export async function processReplyPoll(job: Job<ReplyJobData>) {
   const { workspace_id } = job.data;
 
-  const { runReplyPoll } = await import("../../web/src/lib/outreach/reply-runner");
+  const { runReplyPoll } = await import("../../../web/src/lib/outreach/reply-runner");
   const result = await runReplyPoll(workspace_id, 7);
 
   console.log(`[reply] ws=${workspace_id} inboxes=${result.inboxes} matched=${result.matched} unmatched=${result.unmatched}`);
