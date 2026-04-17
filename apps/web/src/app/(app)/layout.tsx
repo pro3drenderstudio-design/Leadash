e37b6a78cb6165d2c15ad67faeee6c244b9aa913
Malik Adelaja <pro3drenderstudio@gmail.com>
2026-04-17 15:29:51 +0200

    auto-deploy

--- apps/web/src/app/(app)/layout.tsx
+++ apps/web/src/app/(app)/layout.tsx
@@ -9,7 +9,6 @@ import ImpersonationBanner from "@/components/admin/ImpersonationBanner";
 import TrialBanner from "@/components/TrialBanner";
 import BetaBanner from "@/components/BetaBanner";
 import { SidebarProvider } from "@/components/SidebarContext";
-import { cookies } from "next/headers";
 
 async function claimBetaIfApproved(userId: string, email: string, workspaceId: string) {
   const db = createAdminClient();
